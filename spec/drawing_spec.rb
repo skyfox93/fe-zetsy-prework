c94d0c6be8b58e61f97525bf4e860d1854e2bc02
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-18 23:19:23 +0000

    automatically backed up by learn

---
+++ spec/drawing_spec.rb
@@ -0,0 +1,12 @@
+
+describe "drawing" do
+
+  it "a file called drawing exists in the public/img folder" do
+
+    file_names = Dir["css/*"]
+
+    expect(file_names).to include("css/style.css")
+
+  end
+
+end
